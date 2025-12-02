52268ec28f835e72da37f9a8baee0722ed0914e0
Jianlong Chen <jianlong99@gmail.com>
2025-12-02 14:38:04 +0800

    Add js lab javascript-trivia-bot

---
+++ javascript-v9/lab-javascript-trivia-bot/lab-javascript-trivia-bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName ="bot";
+let botLocation = "botLocation";
+let favoriteLanguage = "rust";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `aaa ${favoriteLanguage} xxxx`;
+console.log(codingFact);
+
+codingFact = `bbb ${favoriteLanguage} xxxx`;
+console.log(codingFact);
+
+codingFact = `ccc ${favoriteLanguage} xxxx`;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);

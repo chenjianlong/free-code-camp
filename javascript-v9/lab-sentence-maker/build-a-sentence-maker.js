ff831921cae7ddea156e04c7400cd36cc710765a
Jianlong Chen <jianlong99@gmail.com>
2025-12-02 20:52:06 +0800

    Add js lab sentence-maker

---
+++ javascript-v9/lab-sentence-maker/build-a-sentence-maker.js
@@ -0,0 +1,18 @@
+let adjective = "lovely";
+let noun = "bird";
+let noun2 = "cherry";
+let verb = "fine";
+let place = "tree";
+let adjective2 = "small";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log(`First story: ${firstStory}`);
+
+adjective = "smelly";
+noun = "fox";
+noun2 = "bird";
+verb = "cooked";
+place = "hole";
+adjective2 = "pointed";
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log(`Second story: ${secondStory}`);

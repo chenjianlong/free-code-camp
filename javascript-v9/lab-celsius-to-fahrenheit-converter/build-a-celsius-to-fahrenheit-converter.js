1679f8f6e07e06fd36749c3728de37c011f5e80b
Jianlong Chen <jianlong99@gmail.com>
2025-12-04 14:53:27 +0800

    Add js lab celsius-to-fahrenheit-converter

---
+++ javascript-v9/lab-celsius-to-fahrenheit-converter/build-a-celsius-to-fahrenheit-converter.js
@@ -0,0 +1,7 @@
+const convertCtoF = (celsius) => celsius * (9/5) + 32;
+
+console.log(convertCtoF(0));
+console.log(convertCtoF(-30));
+console.log(convertCtoF(-10));
+console.log(convertCtoF(20));
+console.log(convertCtoF(30));

bf72f0aa9924e37f7b0821069287435194451ea1
Jianlong Chen <jianlong99@gmail.com>
2025-12-12 00:09:16 +0800

    Add js lab all-true-property-validator

---
+++ javascript-v9/lab-all-true-property-validator/build-an-all-true-property-validator.js
@@ -0,0 +1,7 @@
+function truthCheck(collection, pre) {
+    return collection.every(obj => (pre in obj) && obj[pre]);
+  }
+  
+  truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
+  
+  truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name");

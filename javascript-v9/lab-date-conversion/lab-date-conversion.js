18de41ab8104ce58f64d83100031e433d81f6b4e
Jianlong Chen <jianlong99@gmail.com>
2025-12-30 17:07:43 +0800

    Add js lab date-conversion

---
+++ javascript-v9/lab-date-conversion/lab-date-conversion.js
@@ -0,0 +1,21 @@
+const currentDate = new Date();
+const currentDateFormat = `Current Date and Time: ${currentDate}`;
+
+console.log(currentDateFormat);
+
+function formatDateMMDDYYYY (date) {
+  return `Formatted Date (MM/DD/YYYY): ${date.toLocaleDateString("en-US")}`;
+}
+
+console.log(formatDateMMDDYYYY(currentDate));
+
+function formatDateLong(date) {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return `Formatted Date (Month Day, Year): ${date.toLocaleDateString("en-US", options)}`;
+}
+
+console.log(formatDateLong(currentDate));
